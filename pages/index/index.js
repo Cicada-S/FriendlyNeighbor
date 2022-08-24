4fea07ea718af8b51b9673d561fdfba7201f7f5c
Cicada <202586563@qq.com>
2022-08-24 16:25:51 +0800

    Judge whether the user has logged in

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,30 @@
 // index.js
+const db = wx.cloud.database()
+const user = db.collection('User')
+
 Page({
   data: {
 
+  },
+
+  /**
+   * 页面加载
+   */
+   onLoad() {
+    // 判断用户是否登录过
+    this.getUserInfo()
+  },
+
+  // 判断用户是否登录过
+  getUserInfo() {
+    user.get().then(res => {
+      if(res.data.length === 1) {
+        wx.setStorageSync('currentUser', res.data[0])
+      } else {
+        wx.navigateTo({
+          url: '/pages/login/login'
+        })
+      }
+    })
   }
 })
