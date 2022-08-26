511fac292d571b72fe7f1a405652e802f2d03498
Cicada <202586563@qq.com>
2022-08-26 09:35:50 +0800

    me Build page

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,6 +1,15 @@
 // pages/me/me.js
 Page({
   data: {
+    userInfo: {}
+  },
 
+  /**
+   * 页面加载
+   */
+  onLoad() {
+    // 获取本地的用户信息
+    let userInfo = wx.getStorageSync('currentUser')
+    this.setData({ userInfo })
   }
 })
