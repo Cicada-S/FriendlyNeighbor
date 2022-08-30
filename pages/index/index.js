dfd80663a8001d2896953091cf2593fa3ddeaf2d
Cicada <202586563@qq.com>
2022-08-30 10:58:45 +0800

    Process the time of the post into the effect required by the page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,44 +1,14 @@
 // index.js
+import { toDates } from '../../utils/util'
+import { getdate } from '../../utils/pastTime'
+
 const db = wx.cloud.database()
 const user = db.collection('User')
 
 Page({
   data: {
     search: '', // 搜索
-    postList: [
-      {
-        _id: '1',
-        communityId: '1',
-        _openid: 'fsdfadsadda',
-        name: 'Cicada',
-        avatar_url: '/static/images/index/user.jpg',
-        type: 1,
-        beginTime: '8月30 12:00',
-        endTime: '8月30 15:00',
-        departPlace: '汤臣一品',
-        destination: '广州商贸中心',
-        price: 20,
-        phone: '15088888888',
-        remark: '还有两个位置',
-        createTime: '2小时前'
-      },
-      {
-        _id: '2',
-        communityId: '1',
-        _openid: 'gasfadsfafe',
-        name: 'Ting',
-        avatar_url: '/static/images/index/user.jpg',
-        type: 0,
-        beginTime: '8月30 12:00',
-        endTime: '8月30 15:00',
-        departPlace: '汤臣一品',
-        destination: '广州商贸中心',
-        price: 20,
-        phone: '15088888888',
-        remark: '我带了一个22寸的行李箱',
-        createTime: '2小时前'
-      }
-    ], // 帖字列表
+    postList: [], // 帖字列表
   },
 
   /**
@@ -67,10 +37,16 @@ Page({
   // 获取帖子
   async getPostList() {
     let { result } = await wx.cloud.callFunction({name: 'getPostList'})
-    this.setData({
-      postList: result.data
+
+    result.data.forEach(item => {
+      // 处理最早时间和最迟时间
+      item.beginTime = toDates(item.beginTime, 'display')
+      item.endTime = toDates(item.endTime, 'display')
+      // 处理发布时间
+      item.createTime = getdate(item.createTime)
     })
 
+    this.setData({ postList: result.data })
   },
 
   // 跳转到发布帖子
--- utils/util.js
+++ utils/util.js
@@ -1,12 +1,17 @@
 // 将时间戳转换具体时间
-const toDates = times => {
+const toDates = (times, type) => {
   const date = new Date(times)
   const Y = date.getFullYear()
   const M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1)
   const D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate())
   const H = date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
   const Min = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
-  const dateTime = Y + '-' + M + '-' + D + ' ' + H + ':' + Min
+  let dateTime = ''
+  if(type === 'display') {
+    dateTime = M + '月' + D + ' ' + H + ':' + Min
+  } else {
+    dateTime = Y + '-' + M + '-' + D + ' ' + H + ':' + Min
+  }
   return dateTime
 }
 
