add219837fe453aadde282cac4f5aa056049d4e8
Cicada <202586563@qq.com>
2022-09-03 11:49:45 +0800

    Post parent comments child comments

--- pages/post/post.js
+++ pages/post/post.js
@@ -11,15 +11,23 @@ const HitchhikingInformation = db.collection('HitchhikingInformation')
 Page({
   data: {
     bottomLift: app.globalData.bottomLift,
-    postInfo: {},
+    postInfo: {}, // 帖子信息
+    value: '', // 评论
+    focus: false, // 评论框焦点
+    commentType: false, // false 为父评
+    placeholder: '评论...', // 评论框占位符
+    toUid: '', // 被评论者id
+    toNickName: '', // 被评论者昵称
+    replyType: 0, // 0: 子评 1: 回复
+    fatherCommentId: '', // 父评id
     commentList: [
       {
         _id: '123131',
         _openid: '13213412',
         postType: 0,
         postId: '12314',
-        nick_name: 'Cicada',
-        avatar_url: '/static/images/post/user.jpg',
+        nickName: 'Cicada',
+        avatarUrl: '/static/images/post/user.jpg',
         content: '我带了一个26寸的行李箱',
         createTime: '1小时前',
         child_comment: [
@@ -27,11 +35,11 @@ Page({
             _id: '1231',
             fatherCommentId: '123131',
             _openid: '13213412',
-            nick_name: 'Cicada',
-            avatar_url: '/static/images/post/user.jpg',
-            to_uid: '4214423',
-            to_nick_name: 'Ting',
-            reply_type: 0,
+            nickName: 'Cicada',
+            avatarUrl: '/static/images/post/user.jpg',
+            toUid: '4214423',
+            toNickName: 'Ting',
+            replyType: 0,
             content: '斯人若彩虹',
             createTime: '50分钟前'
           }
@@ -77,15 +85,105 @@ Page({
     })
   },
 
+  // 评论
+  hairComment() {
+    console.log(this.data.value)
+
+    if(this.data.commentType) {
+      this.sonComment() // 子级评论
+    }else {
+      this.fatherComment() // 父级评论
+    }
+  },
+
   // 父级评论
-  replyComment(event) {
-    let { id, dataset } = event.currentTarget
-    console.log(id, dataset)
+  fatherComment() {
+    let { value, commentList, postInfo } = this.data
+    let userInfo = wx.getStorageSync('currentUser')
+
+    let data = {
+      postId: postInfo._id,
+      _openid: userInfo._openid,
+      nickName: userInfo.nick_name,
+      avatarUrl: userInfo.avatar_url,
+      createTime: new Date(),
+      content: value
+    }
+
+    wx.cloud.callFunction({
+      name: 'addComment',
+      data
+    }).then(res => {
+      data._id = res.result.data
+      data.createTime = getdate(data.createTime)
+      data.child_comment = [] // 添加上child_comment属性 否则子评会报错
+      commentList.unshift(data)
+      // 更新data
+      this.setData({ commentList, value: '' })
+    })
   },
 
   // 子级评论
+  sonComment() {
+    let { value, commentList, postInfo, toUid, toNickName, replyType, fatherCommentId } = this.data
+    let userInfo = wx.getStorageSync('currentUser')
+
+    let data = {
+      post_id: postInfo._id,
+      _openid: userInfo._openid,
+      nickName: userInfo.nick_name,
+      avatarUrl: userInfo.avatar_url,
+      createTime: new Date(),
+      content: value,
+      agree: 0,
+      toUid,
+      toNickName,
+      replyType,
+      fatherCommentId
+    }
+
+    wx.cloud.callFunction({
+      name: 'addComment',
+      data
+    }).then(res => {
+      data._id = res.result.data
+      data.createTime = getdate(data.createTime)
+      // 将子评添加到指定的父评下
+      commentList.forEach(item => {
+        if(item._id === fatherCommentId) item.child_comment.push(data)
+      })
+      // 更新data
+      this.setData({ commentList, value: '' })
+    })
+  },
+
+  // 点击父级评论 的回调函数
+  replyComment(event) {
+    let { id, dataset } = event.currentTarget
+
+    this.setData({ 
+      focus: true,
+      placeholder: `回复 @${dataset.name}`,
+      commentType: true,
+      toUid: dataset._openid,
+      toNickName: dataset.name,
+      replyType: 0,
+      fatherCommentId: id
+    })
+  },
+
+  // 点击子级评论 的回调函数
   answerComment(event) {
     let { id, dataset } = event.currentTarget
-    console.log(id, dataset)
+
+    this.setData({
+      focus: true,
+      placeholder: `回复 @${dataset.name}`,
+      commentType: true,
+      toUid: dataset._openid,
+      toNickName: dataset.name,
+      replyType: 1,
+      fatherCommentId: id
+    })
   }
 })
