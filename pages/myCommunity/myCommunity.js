2a687b624a287ba2f0b45e81fe5e0780350f3f5a
Cicada <202586563@qq.com>
2022-09-14 14:32:34 +0800

    My community page is completed

--- pages/myCommunity/myCommunity.js
+++ pages/myCommunity/myCommunity.js
@@ -1,7 +1,13 @@
 // pages/myCommunity/myCommunity.js
 Page({
   data: {
-
+    communityInfo: {
+      qrCodePath: '',
+      name: '三水一品',
+      province: '广东省',
+      city: '佛山市',
+      county: '三水区'
+    }
   },
 
   /**
@@ -11,8 +17,8 @@ Page({
     console.log('options', options)
   },
   
-  // 加入小区
-  joinCommunity() {
+  // 更换小区
+  changeCommunity() {
     wx.navigateTo({
       url: '/pages/community/community?type=join'
     })
