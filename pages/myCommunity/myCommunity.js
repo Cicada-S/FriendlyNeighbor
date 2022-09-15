3f34eb4d33ac8d8131d9bbdd7b42fc087f617b9f
Cicada <202586563@qq.com>
2022-09-15 14:25:33 +0800

    My Community Page

--- pages/myCommunity/myCommunity.js
+++ pages/myCommunity/myCommunity.js
@@ -1,6 +1,7 @@
 // pages/myCommunity/myCommunity.js
 const db = wx.cloud.database()
 const UserCommunity = db.collection('UserCommunity')
+const Community = db.collection('Community')
 
 Page({
   data: {
@@ -28,13 +29,21 @@ Page({
   // 获取申请表
   async getUserCommunity() {
     let _openid = wx.getStorageSync('currentUser')._openid
+    // 1. 获取关系表
     let { data } = await UserCommunity.where({_openid}).get()
-    console.log('result', result)
-    /* data.forEach(item => {
+
+    // 2. 遍历获取到的数据 判断数据的status
+    data.forEach(async item => {
       if(!item.status) {
-        this.setData({})
+        // 2.1 如果 status === 0 则根据 communityId 获取社区表
+        let { data } = await Community.where({_id: item.communityId}).get()
+        this.setData({ communityInfo: data[0] })
+      } else {
+        // 2.2 如果 status !== 0 则将数据渲染到页面的申请中模块
+        this.setData({ application: item })
       }
-    }) */
+    })
+
   },
 
   // 更换小区
