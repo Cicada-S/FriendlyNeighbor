30209601750cb81eed80ca6dcfec204f0de5e25c
Cicada <202586563@qq.com>
2022-09-16 18:14:47 +0800

    Separate member and administrator data

--- pages/communityInfo/communityInfo.js
+++ pages/communityInfo/communityInfo.js
@@ -1,34 +1,39 @@
 // pages/communityInfo/communityInfo.js
+const db = wx.cloud.database()
+const UserCommunity = db.collection('UserCommunity')
+const CommunityManager = db.collection('CommunityManager')
+
 Page({
   data: {
     search: '',
-    memberList: [
-      {
-        _id: '1231',
-        nickName: 'Cicada',
-        avatarUrl: '',
-        status: 1
-      },
-      {
-        _id: '231131',
-        nickName: 'Ting',
-        avatarUrl: '',
-        status: 0
-      },
-      {
-        _id: '31231',
-        nickName: 'Cicada',
-        avatarUrl: '',
-        status: 0
-      }
-    ]
+    adminList: [], // 管理员
+    memberList: [] // 普通成员
   },
 
   /**
    * 页面加载
    */
   onLoad(options) {
-    console.log('页面加载', options.id)
+    // 获取小区 成员/管理员
+    this.getCommunityUser(options.id)
+  },
+
+  // 获取小区 成员/管理员
+  async getCommunityUser(id) {
+    let member = await UserCommunity.where({communityId: id, status: 0}).get()
+    let admin = await CommunityManager.where({communityId: id}).get()
+
+    let newMember = [] 
+    member.data.forEach(mItem => {
+      admin.data.forEach(aItem => {
+        if(mItem._openid !== aItem._openid) newMember.push(mItem)
+      })
+    })
+
+    this.setData({
+      memberList: newMember,
+      adminList: admin.data
+    })
   },
 
   // 确定搜索时触发
@@ -41,8 +46,18 @@ Page({
     console.log('event.target.id', event.target.id)
   },
 
-  // 删除的处理函数
+  // 驳回的处理函数
+  onReject(event) {
+    console.log('event.target.id', event.target.id)
+  },
+
+  // 免职的处理函数
   onDetele(event) {
+    console.log('event.target.id', event.target.id)
+  },
+
+  // 踢除的处理函数
+  onKick(event) {
     console.log('event.target.id', event.target.id)
   }
 })
