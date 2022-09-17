b951dd4a75858a74e60618a0598b9e44b697f53e
Cicada <202586563@qq.com>
2022-09-17 11:23:17 +0800

    Get cell members

--- pages/myCommunity/myCommunity.js
+++ pages/myCommunity/myCommunity.js
@@ -1,5 +1,6 @@
 // pages/myCommunity/myCommunity.js
 const db = wx.cloud.database()
+const _ = db.command
 const UserCommunity = db.collection('UserCommunity')
 const Community = db.collection('Community')
 const CommunityManager = db.collection('CommunityManager')
@@ -9,7 +10,9 @@ Page({
     communityInfo: {}, // 所在的小区
     application: {}, // 申请的小区
     adminInfo: {}, // 管理员身份
-    applyAdmin: {} // 申请管理员
+    applyAdmin: {}, // 申请管理员
+    memberList: [], // 成员
+    applyMember: [] // 申请中的成员
   },
 
   /**
@@ -18,6 +21,8 @@ Page({
   onShow() {
     // 获取申请表
     this.getUserCommunity()
+    // 获取成员
+    // this.getMemberList()
   },
 
   // 获取申请表
@@ -36,14 +41,39 @@ Page({
   handleData({data}, type) {
     // 2. 遍历获取到的数据 判断数据的status
     data?.forEach(async item => {
-      if(!item.status && type[0] === 'communityInfo') {
+      if(!item.status) {
         // 2.1 如果 status === 0 则根据 communityId 获取社区表
-        let { data } = await Community.where({_id: item.communityId}).get()
+        if(type[0] === 'communityInfo') {
+          let { data } = await Community.where({_id: item.communityId}).get()
+          this.setData({ [type[0]]: data[0] })
+          return
+        }
         this.setData({ [type[0]]: data[0] })
+        if(type[0] === 'adminInfo') this.getMemberList()
       } else {
         // 2.2 如果 status !== 0 则将数据渲染到页面的申请中模块
         this.setData({ [type[1]]: item })
       }
+    })
+  },
+
+  // 获取成员
+  getMemberList() {
+    UserCommunity.where({
+      communityId: wx.getStorageSync('myCommunity').communityId,
+      _openid: _.eq(this.data.adminInfo._openid)
+    }).orderBy('createTime', 'desc').get()
+    .then(res => {
+      let memberList = []
+      let applyMember = []
+      res.data.forEach(item => {
+        item.status ? applyMember.push(item) : memberList.push(item)
+      })
+
+      this.setData({
+        memberList,
+        applyMember
+      })
     })
   },
 
