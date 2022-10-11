ac2f02db19899b927e026030fc439c90632dc750
Cicada <202586563@qq.com>
2022-10-11 18:30:20 +0800

    Get the data list of idle articles

---
+++ cloud/getIdleItem/index.js
@@ -0,0 +1,52 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+const db = cloud.database()
+const _ = db.command
+const $ = db.command.aggregate
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  try {
+    const result = await db.collection('IdleItem').aggregate()
+    .sort({ createTime: -1 }) // 排序
+    .skip(event.pageSize * (event.pageIndex - 1)) // 跳过第n条开始查询
+    .limit(event.pageSize) // 每次查询的数量
+    .lookup({ // 查询首图的第一张图片
+      from: 'IdleItemVideoImage',
+      let: { idleItemId: '$_id' },
+      pipeline: $.pipeline()
+        .match(_.expr($.and([
+          $.eq(['$IdleItemId', '$$idleItemId']),
+          $.eq(['$type', 0]),
+          $.eq(['$order', 0])
+        ])))
+        .done(),
+      as: 'pathUrl'
+    })
+    .lookup({ // 查询规格
+      from: 'IdleItemSpecification',
+      let: { idleItemId: '$_id' },
+      pipeline: $.pipeline()
+        .match(_.expr($.and([
+          $.eq(['$IdleItemId', '$$idleItemId'])
+        ])))
+        .sort({order: 1})
+        .done(),
+      as: 'specification'
+    }).end()
+
+    return {
+      code: 0,
+      data: result.list,
+      success: true
+    }
+  }
+  catch(err) {
+    console.error('transaction error')
+    return {
+      code: 1,
+      success: false
+    }
+  }
+}
--- pages/oldThings/oldThings.js
+++ pages/oldThings/oldThings.js
@@ -1,55 +1,32 @@
 // pages/oldThings/oldThings.js
 Page({
   data: {
-    listData: [
-      {
-        _id: '12314',
-        name: '测试',
-        price: 123,
-        deliveryMethod: 0, 
-        remark: '打发顺丰达SV艾弗森',
-        phone: '12314124',
-        createTime: '',
-        path: '',
-      },
-      {
-        _id: '12314',
-        name: '测试',
-        price: 123,
-        deliveryMethod: 0, 
-        remark: '个省份顺丰顺丰二妃山润肤乳设的粉色',
-        phone: '12314124',
-        createTime: '',
-        path: '',
-      },
-      {
-        _id: '12314',
-        name: '测试',
-        price: 123,
-        deliveryMethod: 0, 
-        remark: '发的完全对称我去人才完成W大外侧是人工费是否为啊啊啊',
-        phone: '12314124',
-        createTime: '',
-        path: '',
-      },
-      {
-        _id: '12314',
-        name: '测试',
-        price: 123,
-        deliveryMethod: 0, 
-        remark: '安达市大所多撒多群',
-        phone: '12314124',
-        createTime: '',
-        path: '',
-      }
-    ]
+    listData: [], // 列表数据
+    pageIndex: 1, // 当前分页
+    pageSize: 5, // 每次获取数据数量
+    reachBottom: false // 是否到底部
   },
 
   /**
    * 页面加载
    */
   onLoad(options) {
-    console.log('options', options)
+    // 获取闲置物品
+    this.getIdleItem()
+  },
+
+  // 获取闲置物品
+  async getIdleItem() {
+    const { pageIndex, pageSize, reachBottom } = this.data
+
+    if(reachBottom) return
+
+    const data = { pageIndex, pageSize }
+    const { result } = await wx.cloud.callFunction({
+      name: 'getIdleItem',
+      data
+    })
+    this.setData({ listData: result.data })
   },
 
   // 跳转到发布页面
