27ecbb0a37237992e72857693c9d1fdf9ff7ea46
Cicada <202586563@qq.com>
2022-10-14 15:54:15 +0800

    Get data by page

--- pages/oldThings/oldThings.js
+++ pages/oldThings/oldThings.js
@@ -3,7 +3,7 @@ Page({
   data: {
     listData: [], // 列表数据
     pageIndex: 1, // 当前分页
-    pageSize: 10, // 每次获取数据数量
+    pageSize: 5, // 每次获取数据数量
     reachBottom: false // 是否到底部
   },
 
@@ -17,7 +17,7 @@ Page({
 
   // 获取闲置物品
   async getIdleItem(searchValue) {
-    const { pageIndex, pageSize, reachBottom } = this.data
+    const { pageIndex, pageSize, reachBottom, listData } = this.data
 
     if(reachBottom) return
 
@@ -28,8 +28,15 @@ Page({
       name: 'getIdleItem',
       data
     })
+
+    // 如果没有数据了则将 reachBottom 设为 true
+    if(!result.data.length) this.setData({ reachBottom: true })
+
+    let newListData = result.data.length ? result.data : []
+    if(pageIndex > 1) newListData = listData.concat(result.data)
+
     // 更新data
-    this.setData({ listData: result.data })
+    this.setData({ listData: newListData })
   },
 
   // 搜索
@@ -49,5 +56,36 @@ Page({
     wx.navigateTo({
       url: '/pages/release/release'
     })
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+   onPullDownRefresh: function() {
+    this.setData({
+      pageIndex: 1,
+      listData: [],
+      reachBottom: false
+    })
+    this.getIdleItem(this.data.searchValue)
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+    console.log(this.data.reachBottom)
+    if(this.data.reachBottom) return
+
+    this.setData({
+      pageIndex: this.data.pageIndex + 1
+    })
+
+    wx.showToast({
+      title: '加载中...',
+      icon: 'loading',
+      duration: 500
+    })
+    this.getIdleItem(this.data.searchValue)
   }
 })
